c470ebd97df6c34230097ef962ad5a084038c27e
Lorenzo Vuur <epicokapi@gmail.com>
2015-11-06 20:15:16 +0100

    Created space consistency in the project, sorry guys.. I'm a 4 spacer.

--- src/directives/rn-carousel-auto-slide.js
+++ src/directives/rn-carousel-auto-slide.js
@@ -1,31 +1,32 @@
 angular.module('angular-carousel')
 
 .directive('rnCarouselAutoSlide', ['$interval', function($interval) {
-  return {
-    restrict: 'A',
-    link: function (scope, element, attrs) {
-        var stopAutoPlay = function() {
-            if (scope.autoSlider) {
-                $interval.cancel(scope.autoSlider);
-                scope.autoSlider = null;
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            var stopAutoPlay = function() {
+                if (scope.autoSlider) {
+                    $interval.cancel(scope.autoSlider);
+                    scope.autoSlider = null;
+                }
+            };
+            
+            var restartTimer = function() {
+                scope.autoSlide();
+            };
+
+            scope.$watch('carouselIndex', restartTimer);
+
+            if (attrs.hasOwnProperty('rnCarouselPauseOnHover') && attrs.rnCarouselPauseOnHover !== 'false'){
+                element.on('mouseenter', stopAutoPlay);
+                element.on('mouseleave', restartTimer);
             }
-        };
-        var restartTimer = function() {
-            scope.autoSlide();
-        };
 
-        scope.$watch('carouselIndex', restartTimer);
-
-        if (attrs.hasOwnProperty('rnCarouselPauseOnHover') && attrs.rnCarouselPauseOnHover !== 'false'){
-            element.on('mouseenter', stopAutoPlay);
-            element.on('mouseleave', restartTimer);
+            scope.$on('$destroy', function(){
+                stopAutoPlay();
+                element.off('mouseenter', stopAutoPlay);
+                element.off('mouseleave', restartTimer);
+            });
         }
-
-        scope.$on('$destroy', function(){
-            stopAutoPlay();
-            element.off('mouseenter', stopAutoPlay);
-            element.off('mouseleave', restartTimer);
-        });
-    }
-  };
+    };
 }]);
